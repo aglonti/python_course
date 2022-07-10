a52f4b8be7bb259d625b1f71232d2b3c8c55be08
aglonti <108737051+aglonti@users.noreply.github.com>
2022-07-10 13:16:13 +0200

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,30 @@
+exponentiation = 5**5
+print ('5 to the 5th =', exponentiation)
+
+addition = -5+5
+print('-5+5 =', addition)
+
+subtraction = -10-17
+print('-10-17 =', subtraction)
+
+multiplication = 12*4
+print('12*4 =', multiplication)
+
+division = 25/20
+print('25/20 =', division)
+
+integer_division = 15//4
+print('the floor of the division 15/4 =', integer_division)
+
+number_comparison1 = 12>3
+print('12>3 is it true?', number_comparison1)
+
+number_comparison2 = 0==3 and 8>10
+print('0=3 and 8>10 is it true?', number_comparison2)
+
+number_comparison3 = 15+3>5*3
+print('15+3>5*3 is it true?', number_comparison3)
+
+numbers = [2, 10, 31, 57, 80]
+print(numbers, 'is it 10 in numbers?', 10 in numbers)
+print(numbers, '5 is not in numbers.', 5 not in numbers)
